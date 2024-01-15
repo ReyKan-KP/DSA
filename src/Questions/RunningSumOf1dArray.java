6c7666eb9e651614b0e46096e1f8b2edb11846a8
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-15 14:31:52 +0530

    added StringAndStringBuilder

---
+++ src/Questions/RunningSumOf1dArray.java
@@ -0,0 +1,21 @@
+package Questions;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/running-sum-of-1d-array/
+public class RunningSumOf1dArray {
+   static int[] runningSum(int[] nums) {
+     int n = nums.length;
+     int[] rSum = new int[n];
+     rSum[0] = nums[0];
+     for (int i = 1; i < n; i++) {
+       rSum[i] = rSum[i - 1] + nums[i];
+     }
+     return rSum;
+   }
+
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,4};
+    System.out.println(Arrays.toString(runningSum(arr)));
+  }
+}
