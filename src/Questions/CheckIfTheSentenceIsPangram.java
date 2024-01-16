ba0fee8d9a8c0b5d97716e04e0b2af96d70c2046
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-16 22:28:46 +0530

    added leetcode solution of 1832

---
+++ src/Questions/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,23 @@
+package Questions;
+
+public class CheckIfTheSentenceIsPangram {
+    static boolean checkIfPangram(String s) {
+        int n = s.length();
+        int count =0;
+        String alphabet ="qwertyuiopasdfghjklzxcvbnm";
+
+
+        for(int i =0;i<alphabet.length();i++){
+            if(s.contains(String.valueOf(alphabet.charAt(i)))){
+                count++;
+            }
+        }
+        return count >= 26;
+
+    }
+
+  public static void main(String[] args) {
+    String str = "thequickbrownfoxjumpsoverthelazydog";
+    System.out.println(checkIfPangram(str));
+  }
+}
