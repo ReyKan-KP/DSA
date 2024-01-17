a52d3468cba06cda3e6c914dae775556b3b95d78
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-17 10:47:14 +0530

    added leetcode solution of 27

---
+++ src/Questions/RemoveElement.java
@@ -0,0 +1,22 @@
+package Questions;
+
+public class RemoveElement {
+    static int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int k = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        return k;
+    }
+
+
+  public static void main(String[] args) {
+    int[] arr = {3,2,2,3};
+    System.out.println(removeElement(arr, 3));
+  }
+}
