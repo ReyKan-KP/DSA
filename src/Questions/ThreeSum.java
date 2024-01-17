e96ce930bf8b7d72770e54aad144080c09399947
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-17 11:15:13 +0530

    added leetcode solution of 15

---
+++ src/Questions/ThreeSum.java
@@ -0,0 +1,58 @@
+package Questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int target = -nums[i];
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    list.add(triplet);
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = ThreeSum.threeSum(arr);
+
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
