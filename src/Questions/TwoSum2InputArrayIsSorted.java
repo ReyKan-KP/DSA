3534a96287f1830acf89baa763831236c32309d3
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-19 12:31:35 +0530

    added Leetcode solution of 167

---
+++ src/Questions/TwoSum2InputArrayIsSorted.java
@@ -0,0 +1,31 @@
+package Questions;
+
+public class TwoSum2InputArrayIsSorted {
+  static int[] twoSum(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length - 1;
+    int[] ans = new int[2];
+    while (start <= end) {
+      int sum = nums[start] + nums[end];
+
+      if (target < sum) {
+        end = end - 1;
+      } else if (target > sum) {
+        start = start + 1;
+      } else {
+        break;
+      }
+    }
+    ans[0] = start + 1;
+    ans[1] = end + 1;
+
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {2, 7, 11, 15};
+    int target = 9;
+    int[] ans = twoSum(arr, target);
+    System.out.println(ans[0] + " " + ans[1]);
+  }
+}
