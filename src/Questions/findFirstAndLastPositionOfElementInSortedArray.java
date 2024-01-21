54bde9c7f97c0116d047c12989775fbbd2aeb378
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-21 17:17:11 +0530

    added Leetcode solution of 34

--- src/Questions/findFirstAndLastPositionOfElementInSortedArray.java
+++ src/Questions/findFirstAndLastPositionOfElementInSortedArray.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 public class findFirstAndLastPositionOfElementInSortedArray {
 
+
   static int[] searchRange(int[] nums, int target) {
     int[] ans = {-1, -1};
     int startIndex = binarySearch(nums, target, true);
@@ -39,6 +40,53 @@ public class findFirstAndLastPositionOfElementInSortedArray {
     }
     return ans;
   }
+//
+//  int[] searchRange(int[] nums, int target) {
+//    int[] arr = new int[2];
+//    int n = nums.length;
+//    int start = 0;
+//    int end = n - 1;
+//
+//    while (start <= end) {
+//      int mid = start + (end - start) / 2;
+//      if (target > nums[mid]) {
+//        start = mid + 1;
+//      } else if (target < nums[mid]) {
+//        end = mid - 1;
+//      } else {
+//        arr[0] = findStart(nums, target, start, mid);
+//        arr[1] = findEnd(nums, target, mid, end);
+//        return arr;
+//      }
+//    }
+//    return new int[] { -1, -1 };
+//
+//  }
+//
+//  int findStart(int[] nums, int target, int start, int end) {
+//    while (start < end) {
+//      int mid = start + (end - start) / 2;
+//      if (nums[mid] == target) {
+//        end = mid;
+//      } else {
+//        start = mid + 1;
+//      }
+//    }
+//    return start;
+//  }
+//
+//  int findEnd(int[] nums, int target, int start, int end) {
+//    while (start < end) {
+//      int mid = start + (end - start) / 2 + 1;
+//      if (nums[mid] == target) {
+//        start = mid;
+//      } else {
+//        end = mid - 1;
+//      }
+//    }
+//    return end;
+//  }
+//}
 
   public static void main(String[] args) {
 
