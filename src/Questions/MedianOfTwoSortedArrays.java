6b81a4f5391f61f4b3685bbc3f2a3d817e14928d
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-22 13:15:57 +0530

    added Leetcode solution of 4

---
+++ src/Questions/MedianOfTwoSortedArrays.java
@@ -0,0 +1,31 @@
+package Questions;
+//https://leetcode.com/problems/median-of-two-sorted-arrays/
+import java.util.*;
+
+public class MedianOfTwoSortedArrays {
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, nums, 0, nums1.length);
+        System.arraycopy(nums2, 0, nums, nums1.length, nums2.length);
+        Arrays.sort(nums);
+
+        double median;
+        int length = nums.length;
+
+        if (length % 2 == 0) {
+            int middle1 = nums[length / 2 - 1];
+            int middle2 = nums[length / 2];
+            median = (double) (middle1 + middle2) / 2;
+        } else {
+            median = nums[length / 2];
+        }
+
+        return median;
+    }
+
+  public static void main(String[] args) {
+    int[] arr1 = {1, 2};
+    int[] arr2 = {3, 4};
+    System.out.println(findMedianSortedArrays(arr1, arr2));
+  }
+}
