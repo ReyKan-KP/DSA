8977faf7933e3dfd2a792c0a6af56f02c0eb3547
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-23 20:59:15 +0530

    added Leetcode solution of 709

---
+++ src/Questions/ToLowerCase.java
@@ -0,0 +1,14 @@
+package Questions;
+//https://leetcode.com/problems/to-lower-case/
+import java.util.*;
+
+public class ToLowerCase {
+  static String toLowerCase(String s) {
+    return s.toLowerCase();
+  }
+
+  public static void main(String[] args) {
+    String s = "Hello";
+    System.out.println(toLowerCase(s));
+  }
+}
