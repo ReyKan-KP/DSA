509113a22af14d1498f748e12027a3a33db8c939
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-24 20:49:32 +0530

    added Leetcode solution of 1967

--- src/Questions/NumberOfStringsThatAppearAsSubstringsInWord.java
+++ src/Questions/NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -1,5 +1,5 @@
 package Questions;
-
+//https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
 import java.util.*;
 
 public class NumberOfStringsThatAppearAsSubstringsInWord {
