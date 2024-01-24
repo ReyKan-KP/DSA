64a0b83cd4e2cfc546ca62e288bdf2b7ea8371b2
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-24 20:43:14 +0530

    added Leetcode solution of 1967

---
+++ src/Questions/NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,23 @@
+package Questions;
+
+import java.util.*;
+
+public class NumberOfStringsThatAppearAsSubstringsInWord {
+
+  static int numOfStrings(String[] patterns, String word) {
+    int count =0;
+    int n = patterns.length;
+    for(int i =0;i<n;i++){
+      if(word.contains(patterns[i])){
+        count ++;
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    String[] patterns = {"a","abc","bc","d"};
+    String word = "abc";
+    System.out.println(numOfStrings(patterns, word));
+  }
+}
