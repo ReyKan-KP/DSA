b881850f957916b1edfc93eb3b203ea2c2aa2d7f
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-28 10:59:19 +0530

    added Leetcode solution of 35

---
+++ src/Questions/SearchInsertPosition.java
@@ -0,0 +1,29 @@
+package Questions;
+
+// https://leetcode.com/problems/search-insert-position/description/
+import java.util.*;
+
+public class SearchInsertPosition {
+
+  static int searchInsert(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (target < nums[mid]) {
+        end = mid - 1;
+      } else if (target > nums[mid]) {
+        start = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+    return start;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1, 3, 5, 6};
+    int target = 5;
+    System.out.println(searchInsert(nums, target));
+  }
+}
