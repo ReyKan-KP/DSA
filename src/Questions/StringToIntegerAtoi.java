9b20598a4aaeee8cecf6d35ec7d6866561b1f02f
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-01-30 17:08:38 +0530

    added Leetcode solution of 8

---
+++ src/Questions/StringToIntegerAtoi.java
@@ -0,0 +1,41 @@
+package Questions;
+
+// https://leetcode.com/problems/string-to-integer-atoi/description/
+import java.util.*;
+
+public class StringToIntegerAtoi {
+  static int myAtoi(String s) {
+    s = s.trim();
+    if (s.isEmpty()) {
+      return 0;
+    }
+
+    int i = 0;
+    int sign = 1;
+    if (s.charAt(0) == '-') {
+      sign = -1;
+      i++;
+    } else if (s.charAt(0) == '+') {
+      i++;
+    }
+
+    int result = 0;
+
+    while (i < s.length() && Character.isDigit(s.charAt(i))) {
+      int digit = s.charAt(i) - '0';
+      if (result > (Integer.MAX_VALUE - digit) / 10) {
+        return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+      }
+
+      result = result * 10 + digit;
+      i++;
+    }
+
+    return result * sign;
+  }
+
+  public static void main(String[] args) {
+    String s = "42";
+    System.out.println(myAtoi(s));
+  }
+}
